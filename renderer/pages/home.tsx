c4760e29deb909748f978df280c0483c7a26034a
qirtudgus <qirtudgus@naver.com>
2023-01-28 05:35:09 +0900

    feat: 로그인창 스타일링 및 에러처리

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,13 +1,15 @@
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { ref, update } from 'firebase/database';
 import Head from 'next/head';
+import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import styled from 'styled-components';
 import BasicInput from '../components/BasicInput';
 import { SolidButton } from '../components/ButtonGroup';
 import { authService, realtimeDbService } from '../firebaseConfig';
+import logo from '../image/maumTalkLogo.webp';
 
 const Box = styled.div`
   width: 100%;
@@ -16,6 +18,8 @@ const Box = styled.div`
   align-items: center;
   justify-content: center;
   position: fixed;
+  background: #b5d692;
+  flex-direction: column;
 `;
 
 const Wrap = styled.div`
@@ -24,10 +28,15 @@ const Wrap = styled.div`
   justify-content: center;
   align-items: center;
   width: 400px;
+  padding: 30px;
+  border-radius: 10px;
+  box-shadow: ${({ theme }) => theme.boxShadow};
+  background: #fff;
 `;
 
 const RegisterLink = styled.p`
-
+margin: 5px 0 15px 0;
+font-size:14px;
 width: 100%;
 text-align: left;
 & span {
@@ -36,76 +45,119 @@ text-align: left;
 }
 `;
 
-const signInWithEmail = async (email: string, password: string) => {
-  try {
-    // let auth = getAuth();
-    // await setPersistence(authService, browserLocalPersistence).then((res) => {
-    //   //   sessionStorage.setItem('ga', 'hah');
-    //   console.log(res);
-    //   return signInWithEmailAndPassword(authService, email, password);
-    // });
-    await signInWithEmailAndPassword(authService, email, password);
-  } catch (error) {
-    console.log(error);
-    if (
-      error.code === 'auth/user-not-found' &&
-      error.code === 'auth/wrong-password'
-    )
-      alert('아이디 혹은 비밀번호를 확인해주세요');
-    // if (error.code === 'auth/wrong-password') alert('비밀번호가 틀렸습니다.');
-    return error;
+const Logo = styled.div`
+  margin-bottom: 15px;
+
+  & p {
+    margin-top: 5px;
+    text-align: center;
+    font-size: 14px;
+    color: #555;
   }
-};
+`;
 
 const Login = () => {
   const router = useRouter();
-
-  let login = () => {
-    // let email = emailRef.current.value;
-    // let password = passwordRef.current.value;
-
-    const emailInput = emailRef.current.lastChild as HTMLInputElement;
-    const passwordInput = passwordRef.current.lastChild as HTMLInputElement;
-    console.log(emailInput.value, passwordInput.value);
-    signInWithEmail(emailInput.value, passwordInput.value).then((res) => {
-      //로그인 결과가 성공적일경우와 에러일 경우 분기
-      if (res) {
-        alert('로그인에 실패하였습니다. 다시 시도해주세요!');
-        return;
-      } else {
-        const connectedRef = ref(
-          realtimeDbService,
-          `userList/${authService.currentUser.uid}`,
-        );
-        update(connectedRef, { isOn: true });
-        router.push('/main');
-      }
-    });
-  };
-
   const emailRef = useRef<HTMLDivElement>();
   const passwordRef = useRef<HTMLDivElement>();
+  const [isLoginError, setIsLoginError] = useState(false);
+  const [isLoginText, setIsLoginText] = useState('');
+  const [isPasswordError, setIsPasswordError] = useState(false);
+  const [isPasswordText, setIsPasswordText] = useState('');
+
+  const signInWithEmail = async (email: string, password: string) => {
+    try {
+      await signInWithEmailAndPassword(authService, email, password);
+    } catch (error) {
+      return error;
+    }
+  };
+
+  const login = () => {
+    const emailInput = emailRef.current.firstChild as HTMLInputElement;
+    const passwordInput = passwordRef.current.firstChild as HTMLInputElement;
+    signInWithEmail(emailInput.value, passwordInput.value).then(
+      (loginResult) => {
+        //로그인 결과가 성공적일경우와 에러일 경우 분기
+        // 성공이면 loginResult에 undifined가, 실패면 error 객체가 들어있다
+        if (loginResult !== undefined) {
+          let errorCode = loginResult.code;
+          console.log(errorCode);
+          switch (errorCode) {
+            case 'auth/user-not-found': {
+              setIsLoginError(true);
+              setIsLoginText('존재하지않는 사용자입니다.');
+              emailInput.focus();
+              break;
+            }
+
+            case 'auth/invalid-email': {
+              setIsLoginError(true);
+              setIsLoginText('이메일 양식을 확인해주세요!');
+              emailInput.focus();
+              break;
+            }
+            case 'auth/wrong-password': {
+              setIsLoginError(false);
+              setIsLoginText('');
+              setIsPasswordError(true);
+              setIsPasswordText('비밀번호를 확인해주세요!');
+              passwordInput.focus();
+              break;
+            }
+            case 'auth/internal-error': {
+              setIsLoginError(false);
+              setIsLoginText('');
+              setIsPasswordError(true);
+              setIsPasswordText('비밀번호를 확인해주세요!');
+              passwordInput.focus();
+              break;
+            }
+            default: {
+              setIsLoginError(true);
+              setIsPasswordError(false);
+              setIsLoginText('이메일 혹은 비밀번호를 확인해주세요!');
+              emailInput.focus();
+              break;
+            }
+          }
+
+          return;
+        } else {
+          const connectedRef = ref(
+            realtimeDbService,
+            `userList/${authService.currentUser.uid}`,
+          );
+          update(connectedRef, { isOn: true });
+          router.push('/main');
+        }
+      },
+    );
+  };
 
   return (
     <>
       <Head>
         <title>maumTalk - 로그인</title>
       </Head>
-      <button onClick={login}>로그인</button>
       <Box>
         <Wrap>
+          <Logo>
+            <Image src={logo} />
+            <p>마음톡에 오신걸 환영해요!</p>
+          </Logo>
           <BasicInput
             ref={emailRef}
             placeholderValue='이메일'
-            // isError={loginResult.idValue}
-            // statusText={loginResult.idStatusText}
+            isError={isLoginError}
+            statusText={isLoginText}
           ></BasicInput>
           <BasicInput
             type='password'
             ref={passwordRef}
             placeholderValue='비밀번호'
-            // isError={loginResult.passwordValue}
-            // statusText={loginResult.passwordStatusText}
+            isError={isPasswordError}
+            statusText={isPasswordText}
           ></BasicInput>
           <RegisterLink>
             아직 회원이 아니시라면?...
@@ -117,6 +169,7 @@ const Login = () => {
             BasicButtonValue='로그인'
             width={400}
             OnClick={login}
+            marginTop={0}
           ></SolidButton>
         </Wrap>
       </Box>
