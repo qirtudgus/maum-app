10c58c65befe3e9a36403b465d0cf75690340acc
qirtudgus <qirtudgus@naver.com>
2023-01-28 08:58:35 +0900

    feat: 레이아웃 구조 변경
    1. 이제 안쓰는 테스트용 버튼 제거
    2. 스크롤을 각 목록마다 부여함
    3. 사이드메뉴 추가

--- renderer/pages/main.tsx
+++ renderer/pages/main.tsx
@@ -27,7 +27,8 @@ const Wrap = styled.div`
 const MenuWrap = styled.div`
   max-width: 300px;
   border-right: 1px solid #eee;
-  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
 `;
 
 const ChatWrap = styled.div`
@@ -36,6 +37,13 @@ const ChatWrap = styled.div`
   min-width: 400px;
   display: flex;
   flex-direction: column;
+`;
+
+const SideBarWrap = styled.div`
+  width: 50px;
+  flex-shrink: 0;
+  height: 100%;
+  background: #eee;
 `;
 
 function Home() {
@@ -52,10 +60,7 @@ function Home() {
   채팅방 내용은 클릭 한 상대방의 uid가 내 uid에 들어있는지 확인.
   2-1.들어있다면 한 층 더 들어가서, chatRoomsUid를 습득하고
   /oneToOneChatRooms/${chatRoomsUid} 에서 데이터를 가져와서 나열시킨다.
-  2-2. 들어있지않다면 chatRoomsUid를 생성 후 데이터를 가져와서 나열시킨다.
-
-
-   
+  2-2. 들어있지않다면 chatRoomsUid를 생성 후 데이터를 가져와서 나열시킨다.   
   */
 
   const [isStartChat, setIsStartChat] = useState(false);
@@ -74,7 +79,6 @@ function Home() {
         //로그아웃시 해당 유저의 상태를 false로 만들어 렌더링 시 회색(?)으로 뜨게 하자
         //fix:유저구조가 변경되어 안쓴다.
         // set(myConnectionsRef, false);
-
         //로그인 시 미리 할당되어있는 uid값을 사용해야함 여기서 새로 참조하면 이미 로그아웃 후라 값이 없음.
         if (uid) {
           const myConnectionsRef = ref(realtimeDbService, `userList/${uid}`);
@@ -101,168 +105,20 @@ function Home() {
       <Head>
         <title>Home - Nextron (with-typescript)</title>
       </Head>
+      <SideBarWrap>
+        <div>메뉴</div>
+      </SideBarWrap>
       <MenuWrap>
         {uid && <div>환영합니다 {displayName}님!</div>}
         {uid ? (
           <button onClick={userSignOut}>로그아웃</button>
         ) : (
           <>
-            <Link href='/login'>로그인</Link>
+            <Link href='/home'>로그인</Link>
             <Link href='/register'>회원가입</Link>
           </>
         )}
 
-        {/* <button
-          onClick={() => {
-            console.log('로그인 상태확인');
-            const uid: string = authService.currentUser?.uid;
-            console.log(authService.currentUser);
-            // console.log(authService.currentUser.displayName);
-            console.log(uid);
-            // return uid;
-          }}
-        >
-          로그인 상태 확인
-        </button> */}
-        {/* <button
-          onClick={() => {
-            push(locate, {
-              uid: authService.currentUser.uid,
-              name: authService.currentUser.displayName,
-
-              createdAt: Timestamp.fromDate(new Date()),
-            }).then((res) => {
-              console.log(res);
-            });
-          }}
-        >
-          실시간db 생성
-        </button> */}
-        <button
-          onClick={() => {
-            onAuthStateChanged(authService, (user) => {
-              console.log(user);
-
-              console.log(authService.currentUser);
-              //로그인중이면 유저정보
-              //로그아웃이면 null 반환
-            });
-          }}
-        >
-          로그인 상태 확인
-        </button>
-        <button
-          onClick={async () => {
-            const 내채팅방 = ref(
-              realtimeDbService,
-              `userList/${authService.currentUser.uid}/myGroupChatList/groupChatUid`,
-            );
-
-            //해당 유저의 채팅방 길이를 가져온다.
-            const 데이터전 = (await get(내채팅방)).size;
-            //길이에 고유값을 추가한다.
-            update(내채팅방, { [데이터전]: 'test' });
-          }}
-        >
-          그룹채팅방 고유id 하나 업데이트
-        </button>
-        <button onClick={getUserList}>현재 접속 유저 확인</button>
-        <button
-          onClick={async () => {
-            //push로 하면 고유키때문에 너무 번거로워진다.
-            //get을 통해 배열화 시킨 후
-            //해당 배열에 추가될 그룹코드를 push()해준 뒤에
-            //set을 통해 새로운 배열로 덮어씌워준다.
-            let 그룹채팅 = ref(
-              realtimeDbService,
-              `userList/${uid}/myGroupChatList`,
-            );
-
-            console.log('그룹챗에 id 넣기');
-            if (authService.currentUser?.uid) {
-              // let 현재그룹채팅 = await get(그룹채팅);
-              let 현재그룹채팅배열: any[] = await (await get(그룹채팅)).val()
-                ?.groupChatUid;
-              if (현재그룹채팅배열) {
-                let 고유번호 = createChatUid();
-                console.log(현재그룹채팅배열);
-                //추가마다 배열을 복사해와서 추가해준 뒤 set한다.
-                let 갱신채팅배열 = [...현재그룹채팅배열, 고유번호];
-                console.log(갱신채팅배열);
-                set(그룹채팅, {
-                  groupChatUid: 갱신채팅배열,
-                });
-              } else {
-                let 고유번호 = createChatUid();
-                set(그룹채팅, {
-                  groupChatUid: [고유번호],
-                });
-              }
-            } else {
-              alert('로그인 후 이용해주세요.');
-            }
-          }}
-        >
-          userList에 그룹챗id 넣기
-        </button>
-        <button
-          onClick={async () => {
-            if (authService.currentUser?.uid) {
-              let 그룹챗정보 = await get(
-                ref(realtimeDbService, `userList/${uid}/myGroupChatList`),
-              );
-              //유저의 채팅리스트 코드가 들어있음.
-              let 그룹챗리스트: groupChatListInterface[] = Object.values(
-                그룹챗정보.val(),
-              );
-              //순회하면서 렌더링해주면 될듯
-
-              console.log(그룹챗리스트[0]);
-            }
-          }}
-        >
-          내 그룹챗 정보 불러오기
-        </button>
-        <button
-          onClick={async () => {
-            //불러와서 고유키를 삭제한 배열을 가공하여 다시 집어넣어준다.
-            if (authService.currentUser?.uid) {
-              let 그룹챗정보 = await get(
-                ref(
-                  realtimeDbService,
-                  `userList/${uid}/myGroupChatList/groupChatUid`,
-                ),
-              );
-
-              let 그룹챗배열: string[] = [...그룹챗정보.val()];
-
-              console.log(그룹챗배열);
-              let 삭제인덱스 = 그룹챗배열.indexOf('0pjezkg9ij');
-              //splice는 삭제된 배열을 반환하고 원본배열에서 변경된다.
-              //배열에 없는 uid가 들어오면 인덱스에 -1이 반환되어 맨 뒤 배열이 삭제된다.
-              //배열에 없는 경우에 대한 에러 처리를 하자
-              if (삭제인덱스 !== -1) {
-                if (confirm(`이 방에서 나가시겠습니까?`)) {
-                  그룹챗배열.splice(삭제인덱스, 1);
-                  let 그룹채팅 = ref(
-                    realtimeDbService,
-                    `userList/${uid}/myGroupChatList`,
-                  );
-
-                  set(그룹채팅, {
-                    groupChatUid: 그룹챗배열,
-                  });
-                }
-              } else {
-                alert('존재하지않는 채팅방입니다.');
-              }
-
-              console.log(그룹챗배열);
-            }
-          }}
-        >
-          내 특정 그룹챗 삭제하기
-        </button>
         {
           <>
             <OnUserList
@@ -301,3 +157,143 @@ function Home() {
 }
 
 export default Home;
+
+{
+  /* <button
+onClick={() => {
+  console.log('로그인 상태확인');
+  const uid: string = authService.currentUser?.uid;
+  console.log(authService.currentUser);
+  // console.log(authService.currentUser.displayName);
+  console.log(uid);
+  // return uid;
+}}
+>
+로그인 상태 확인
+</button>
+<button
+onClick={() => {
+  onAuthStateChanged(authService, (user) => {
+    console.log(user);
+
+    console.log(authService.currentUser);
+    //로그인중이면 유저정보
+    //로그아웃이면 null 반환
+  });
+}}
+>
+로그인 상태 확인
+</button>
+<button
+onClick={async () => {
+  const 내채팅방 = ref(
+    realtimeDbService,
+    `userList/${authService.currentUser.uid}/myGroupChatList/groupChatUid`,
+  );
+
+  //해당 유저의 채팅방 길이를 가져온다.
+  const 데이터전 = (await get(내채팅방)).size;
+  //길이에 고유값을 추가한다.
+  update(내채팅방, { [데이터전]: 'test' });
+}}
+>
+그룹채팅방 고유id 하나 업데이트
+</button>
+<button onClick={getUserList}>현재 접속 유저 확인</button>
+<button
+onClick={async () => {
+  //push로 하면 고유키때문에 너무 번거로워진다.
+  //get을 통해 배열화 시킨 후
+  //해당 배열에 추가될 그룹코드를 push()해준 뒤에
+  //set을 통해 새로운 배열로 덮어씌워준다.
+  let 그룹채팅 = ref(
+    realtimeDbService,
+    `userList/${uid}/myGroupChatList`,
+  );
+
+  console.log('그룹챗에 id 넣기');
+  if (authService.currentUser?.uid) {
+    // let 현재그룹채팅 = await get(그룹채팅);
+    let 현재그룹채팅배열: any[] = await (await get(그룹채팅)).val()
+      ?.groupChatUid;
+    if (현재그룹채팅배열) {
+      let 고유번호 = createChatUid();
+      console.log(현재그룹채팅배열);
+      //추가마다 배열을 복사해와서 추가해준 뒤 set한다.
+      let 갱신채팅배열 = [...현재그룹채팅배열, 고유번호];
+      console.log(갱신채팅배열);
+      set(그룹채팅, {
+        groupChatUid: 갱신채팅배열,
+      });
+    } else {
+      let 고유번호 = createChatUid();
+      set(그룹채팅, {
+        groupChatUid: [고유번호],
+      });
+    }
+  } else {
+    alert('로그인 후 이용해주세요.');
+  }
+}}
+>
+userList에 그룹챗id 넣기
+</button>
+<button
+onClick={async () => {
+  if (authService.currentUser?.uid) {
+    let 그룹챗정보 = await get(
+      ref(realtimeDbService, `userList/${uid}/myGroupChatList`),
+    );
+    //유저의 채팅리스트 코드가 들어있음.
+    let 그룹챗리스트: groupChatListInterface[] = Object.values(
+      그룹챗정보.val(),
+    );
+    //순회하면서 렌더링해주면 될듯
+
+    console.log(그룹챗리스트[0]);
+  }
+}}
+>
+내 그룹챗 정보 불러오기
+</button>
+<button
+onClick={async () => {
+  //불러와서 고유키를 삭제한 배열을 가공하여 다시 집어넣어준다.
+  if (authService.currentUser?.uid) {
+    let 그룹챗정보 = await get(
+      ref(
+        realtimeDbService,
+        `userList/${uid}/myGroupChatList/groupChatUid`,
+      ),
+    );
+
+    let 그룹챗배열: string[] = [...그룹챗정보.val()];
+
+    console.log(그룹챗배열);
+    let 삭제인덱스 = 그룹챗배열.indexOf('0pjezkg9ij');
+    //splice는 삭제된 배열을 반환하고 원본배열에서 변경된다.
+    //배열에 없는 uid가 들어오면 인덱스에 -1이 반환되어 맨 뒤 배열이 삭제된다.
+    //배열에 없는 경우에 대한 에러 처리를 하자
+    if (삭제인덱스 !== -1) {
+      if (confirm(`이 방에서 나가시겠습니까?`)) {
+        그룹챗배열.splice(삭제인덱스, 1);
+        let 그룹채팅 = ref(
+          realtimeDbService,
+          `userList/${uid}/myGroupChatList`,
+        );
+
+        set(그룹채팅, {
+          groupChatUid: 그룹챗배열,
+        });
+      }
+    } else {
+      alert('존재하지않는 채팅방입니다.');
+    }
+
+    console.log(그룹챗배열);
+  }
+}}
+>
+내 특정 그룹챗 삭제하기
+</button> */
+}
