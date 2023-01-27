5347778bfb810f019f77ed9fd61f3d0b8bfc95b6
qirtudgus <qirtudgus@naver.com>
2023-01-28 08:42:22 +0900

    feat: strictmode, styledComponent 설정추가

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  reactStrictMode: false,
+  compiler: {
+    styledComponents: { displayName: true, ssr: true, minify: true },
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.target = 'electron-renderer';
