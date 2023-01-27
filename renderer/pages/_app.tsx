28b9af72309a060235891208bbaae490b9a67370
qirtudgus <qirtudgus@naver.com>
2023-01-28 04:03:35 +0900

    feat: ThemeProvider 적용

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import GlobalStyle from '../components/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import theme from '../components/GlobalTheme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <React.Fragment>
-      <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-      </Head>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </React.Fragment>
+    <ThemeProvider theme={theme}>
+      <>
+        <Head>
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
+        </Head>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </>
+    </ThemeProvider>
   );
 }
 
