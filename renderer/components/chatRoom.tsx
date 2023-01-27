042662596b0a87fcc28c28966b93f5877b6f05e2
qirtudgus <qirtudgus@naver.com>
2023-01-28 08:40:18 +0900

    feat: 메시지인풋 컴포넌트화에 따른 코드 정리

--- renderer/components/chatRoom.tsx
+++ renderer/components/chatRoom.tsx
@@ -7,40 +7,7 @@ import { authService, realtimeDbService } from '../firebaseConfig';
 import ChatRoomHeaderTitle from './ChatRoomHeaderTitle';
 import LoadingSpinner from './LoadingSpinner';
 import MessageContainerFront from './messageContainer';
-
-const MessageInput = styled.div`
-  width: 100%;
-  border: 1px solid#eee;
-  padding: 5px;
-  display: flex;
-  align-items: center;
-  & > input {
-    padding: 10px 10px;
-    width: 100%;
-    border: none;
-  }
-
-  & > input:focus {
-    outline: none;
-  }
-  & > button {
-    cursor: pointer;
-    border: none;
-    border-radius: 4px;
-    flex-shrink: 0;
-    padding: 10px;
-    background: ${({ theme }) => theme.colors.main};
-  }
-  & > button:hover {
-    background: ${({ theme }) => theme.colors.mainHoverColor};
-  }
-`;
-
-const Footer = styled.div`
-  width: 100%;
-  height: 100px;
-  background-color: #fff;
-`;
+import SendMessageInput from './SendMessageInput';
 
 export function convertDate(time) {
   //time should be server timestamp seconds only
@@ -62,34 +29,7 @@ const ChatRoom = ({
   setIsStartGroupChat: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
   const [chatList, setChatList] = useState([]);
-  const messageInputRef = useRef<HTMLInputElement>();
-  const messageSendRef = useRef<HTMLButtonElement>();
-  const [isOnlySpaceInputValue, setIsOnlySpaceInputValue] = useState(true);
   const [isChatLoading, setIsChatLoading] = useState(false);
-
-  const [inputValue, setInputValue] = useState('');
-
-  const blank_pattern = /^\s+\s+$/g;
-  const SendMessage = async () => {
-    //저장할 경로
-    const 채팅저장경로 = ref(
-      realtimeDbService,
-      `oneToOneChatRooms/${chatRoomInfo.chatRoomUid}/chat`,
-    );
-
-    await push(채팅저장경로, {
-      displayName: authService.currentUser.displayName,
-      uid: authService.currentUser.uid,
-      message: messageInputRef.current.value,
-
-      createdAt: convertDate(Timestamp.fromDate(new Date()).seconds),
-    });
-
-    //메시지 작성 후 비워주기
-    messageInputRef.current.focus();
-    setInputValue('');
-    setIsOnlySpaceInputValue(true);
-  };
 
   //useEffect onValue로 채팅을 계속 가져와야함
   useEffect(() => {
@@ -113,7 +53,6 @@ const ChatRoom = ({
       //언마운트시 해당 경로에 대한 관찰자를 off해주면 왔다갔다해도 onValue가 한번씩만 호출됨 (onValue가 쌓이는걸 방지)
       off(채팅경로);
       setIsChatLoading(false);
-
       console.log('채팅방을 나갔습니다.');
     };
   }, [chatRoomInfo.chatRoomUid]);
@@ -141,39 +80,10 @@ const ChatRoom = ({
       ) : (
         <LoadingSpinner />
       )}
-
-      <MessageInput>
-        <input
-          ref={messageInputRef}
-          placeholder='메시지를 입력해주세요'
-          // defaultValue={''}
-          value={inputValue}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setInputValue(e.currentTarget.value);
-            if (
-              e.currentTarget.value === ' ' ||
-              e.currentTarget.value.length === 0 ||
-              blank_pattern.test(e.currentTarget.value)
-            ) {
-              setIsOnlySpaceInputValue(true);
-              // return;
-            } else {
-              setIsOnlySpaceInputValue(false);
-            }
-          }}
-          onKeyDown={(e: React.KeyboardEvent) => {
-            if (e.key === 'Enter') messageSendRef.current.click();
-          }}
-        ></input>
-        <button
-          ref={messageSendRef}
-          disabled={isOnlySpaceInputValue}
-          onClick={SendMessage}
-        >
-          전송
-        </button>
-      </MessageInput>
-      <Footer />
+      <SendMessageInput
+        chatRoomInfo={chatRoomInfo}
+        isOneToOneOrGroup='oneToOne'
+      />
     </>
   );
 };
