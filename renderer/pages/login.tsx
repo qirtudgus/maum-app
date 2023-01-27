4c23e10ac74f1c9834ddfb07d9012ccdd19c0dc2
qirtudgus <qirtudgus@naver.com>
2023-01-27 15:04:07 +0900

    feat: 로그인중 비밀번호 틀렸을 때 케이스 추가

--- renderer/pages/login.tsx
+++ renderer/pages/login.tsx
@@ -1,4 +1,4 @@
-import { setPersistence, signInWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import { push, ref, set, update } from 'firebase/database';
 import { Timestamp } from 'firebase/firestore';
 import Link from 'next/link';
@@ -13,15 +13,23 @@ import {
 
 const Input = styled.input``;
 
-const signInWithEmail = async (id, password) => {
+const signInWithEmail = async (email: string, password: string) => {
   try {
-    await signInWithEmailAndPassword(authService, id, password).then((res) => {
-      console.log(res);
-    });
+    // let auth = getAuth();
+    // await setPersistence(authService, browserLocalPersistence).then((res) => {
+    //   //   sessionStorage.setItem('ga', 'hah');
+    //   console.log(res);
+    //   return signInWithEmailAndPassword(authService, email, password);
+    // });
+    await signInWithEmailAndPassword(authService, email, password);
   } catch (error) {
     console.log(error);
-    if (error.code === 'auth/user-not-found')
+    if (
+      error.code === 'auth/user-not-found' &&
+      error.code === 'auth/wrong-password'
+    )
       alert('아이디 혹은 비밀번호를 확인해주세요');
+    // if (error.code === 'auth/wrong-password') alert('비밀번호가 틀렸습니다.');
     return error;
   }
 };
@@ -30,31 +38,35 @@ const Login = () => {
   const router = useRouter();
 
   let login = () => {
-    let id = idRef.current.value;
+    let email = emailRef.current.value;
     let password = passwordRef.current.value;
-    console.log(id, password);
-    signInWithEmail(id, password).then((res) => {
-      const connectedRef = ref(
-        realtimeDbService,
-        `userList/${authService.currentUser.uid}`,
-      );
-
-      update(connectedRef, { isOn: true });
-
-      router.push('/home');
+    console.log(email, password);
+    signInWithEmail(email, password).then((res) => {
+      //로그인 결과가 성공적일경우와 에러일 경우 분기
+      if (res) {
+        alert('로그인에 실패하였습니다. 다시 시도해주세요!');
+        return;
+      } else {
+        const connectedRef = ref(
+          realtimeDbService,
+          `userList/${authService.currentUser.uid}`,
+        );
+        update(connectedRef, { isOn: true });
+        router.push('/home');
+      }
     });
   };
 
-  const idRef = useRef<HTMLInputElement>();
+  const emailRef = useRef<HTMLInputElement>();
   const passwordRef = useRef<HTMLInputElement>();
 
   return (
     <>
       <Link href={'/home'}>홈</Link>
       아이디
-      <Input ref={idRef} defaultValue='qwer@naver.com'></Input>
+      <Input ref={emailRef} defaultValue='qwer@naver.com'></Input>
       비밀번호
-      <Input ref={passwordRef} defaultValue='qwer12'></Input>
+      <Input ref={passwordRef} type='password' defaultValue='qwer12'></Input>
       <button onClick={login}>로그인</button>
     </>
   );
