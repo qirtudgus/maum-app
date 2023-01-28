56e452d08bd6e3b47fb344bcbeb0cc96796c0e35
qirtudgus <qirtudgus@naver.com>
2023-01-28 09:57:40 +0900

    feat: 이메일 저장 기능 추가
    1. 체크돼있으면 매번 로그인한 값으로 스토리지 갱신
    2. 가입 시에는 가입한 값으로 인풋 할당
    3. 체크해제 후 로그인 시 스토리지 삭제

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -60,6 +60,7 @@ const Login = () => {
   const router = useRouter();
   const emailRef = useRef<HTMLDivElement>();
   const passwordRef = useRef<HTMLDivElement>();
+  const emailSaveRef = useRef<HTMLInputElement>();
   const [isLoginError, setIsLoginError] = useState(false);
   const [isLoginText, setIsLoginText] = useState('');
   const [isPasswordError, setIsPasswordError] = useState(false);
@@ -76,18 +77,45 @@ const Login = () => {
   };
 
   useLayoutEffect(() => {
-    if (방금가입한메일) {
-      const emailInput = emailRef.current.firstChild as HTMLInputElement;
+    //로컬스토리지 mail 체크 후 input에 저장
+    //저장은 해놨고, 방금 가입을 했으면 방금 가입한 메일을 인풋에
+    //저장만 해놨으면 인풋에
+    //저장 안해놨고 가입했으면 가입을 인풋에
+    const isMailSave = localStorage.getItem('mail');
+    const emailInput = emailRef.current.firstChild as HTMLInputElement;
+    const passwordInput = passwordRef.current.firstChild as HTMLInputElement;
+    if (isMailSave && 방금가입한메일) {
+      emailInput.value = 방금가입한메일;
+      emailSaveRef.current.checked = true;
       emailInput.focus();
-    } else {
-      const emailInput = emailRef.current.firstChild as HTMLInputElement;
+      passwordInput.focus();
+      // emailInput.placeholder = '';
+    } else if (isMailSave && !방금가입한메일) {
+      emailInput.value = isMailSave;
+      emailSaveRef.current.checked = true;
       emailInput.focus();
+      passwordInput.focus();
+    } else if (!isMailSave && 방금가입한메일) {
+      emailInput.value = 방금가입한메일;
+      emailInput.focus();
+      passwordInput.focus();
     }
   }, []);
 
   const login = () => {
     const emailInput = emailRef.current.firstChild as HTMLInputElement;
     const passwordInput = passwordRef.current.firstChild as HTMLInputElement;
+    const isEmailSave = emailSaveRef.current.checked;
+    //로그인 시 check가 true면 로컬스토리지에 이메일을 저장시키고,
+    //check가 false면 로컬스토리지에 이메일을 삭제시킨다.
+    if (isEmailSave) {
+      //로컬스토리지에 저장
+      localStorage.setItem('mail', emailInput.value);
+    } else {
+      //로컬스토리지에서 삭제
+      localStorage.removeItem('mail');
+    }
+
     signInWithEmail(emailInput.value, passwordInput.value).then(
       (loginResult) => {
         //로그인 결과가 성공적일경우와 에러일 경우 분기
@@ -178,6 +206,17 @@ const Login = () => {
               <Link href={'/__register'}> 회원가입</Link>
             </span>
           </RegisterLink>
+          <div>
+            이메일 저장
+            <input
+              onChange={(e) => {
+                console.log(emailSaveRef.current.checked);
+              }}
+              ref={emailSaveRef}
+              type='checkbox'
+            ></input>
+          </div>
+
           <SolidButton
             BasicButtonValue='로그인'
             width={400}
