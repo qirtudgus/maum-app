16df50b0dc796549db24b49184d5686ae6414de5
qirtudgus <qirtudgus@naver.com>
2023-01-29 02:53:43 +0900

    feat: 레이아웃 추가

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -4,17 +4,18 @@ import type { AppProps } from 'next/app';
 import GlobalStyle from '../components/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import theme from '../components/GlobalTheme';
+import Layout from '../layout/layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <>
-        <Head>
-          <meta name='viewport' content='width=device-width, initial-scale=1' />
-        </Head>
-        <GlobalStyle />
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
+      <GlobalStyle />
+      <Layout>
         <Component {...pageProps} />
-      </>
+      </Layout>
     </ThemeProvider>
   );
 }
