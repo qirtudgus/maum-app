1c482241b9ad6cc94616c2d927b9b312cebe17ce
qirtudgus <qirtudgus@naver.com>
2023-01-29 02:55:56 +0900

    feat: 레이아웃 컴포넌트 생성
    로그인과 회원가입에서는 레이아웃 안보이도록 구현

---
+++ renderer/layout/layout.tsx
@@ -0,0 +1,48 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import styled from 'styled-components';
+import { authService } from '../firebaseConfig';
+import SideBar from './sideBar';
+
+const LayoutDiv = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  border-top: 1px solid ${({ theme }) => theme.colors.borderColor};
+`;
+
+const Main = styled.div`
+  width: 100%;
+  position: relative;
+`;
+
+export default function Layout({ children }) {
+  const router = useRouter();
+  const checkNotLayoutPathname = (): boolean => {
+    let isLayoutRendering = false;
+    const notLayout = ['/home', '/register'];
+    if (notLayout.includes(router.pathname)) {
+      isLayoutRendering = true;
+    }
+    return isLayoutRendering;
+  };
+
+  useEffect(() => {
+    //로그인정보가 없을경우 로그인창으로 이동
+    if (authService.currentUser === null) router.push('/home');
+  }, []);
+
+  return (
+    <>
+      {!checkNotLayoutPathname() ? (
+        <LayoutDiv>
+          <SideBar />
+          <Main>{children}</Main>
+        </LayoutDiv>
+      ) : (
+        <Main>{children}</Main>
+      )}
+    </>
+  );
+}
