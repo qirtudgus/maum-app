07be30fb34c735a670e42a8147ce3d04316b6207
qirtudgus <qirtudgus@naver.com>
2023-01-29 04:41:26 +0900

    feat: 그룹채팅생성 모달 분할 후 불필요 코드 주석처리

--- renderer/components/newGroupChatList.tsx
+++ renderer/components/newGroupChatList.tsx
@@ -12,6 +12,7 @@ import {
   getGroupChatRoomsUidToTitleFunc,
 } from '../firebaseConfig';
 import { convertDate } from '../utils/convertDate';
+import InviteGroupChatModal from './inviteGroupChatModal';
 
 export const GroupListTitle = styled.div`
   display: flex;
@@ -72,71 +73,73 @@ const GroupListLi = styled.li`
 `;
 
 const AddGroupChatModal = styled.div`
-  top: 300px;
-  left: 400px;
+  top: 0;
+  left: 0;
   right: 0;
   position: fixed;
   margin: 0 auto;
   width: 300px;
   height: 300px;
   background: #eee;
+  z-index: 10;
+  overflow-y: auto;
   /* display: flex; */
   /* flex-direction: column; */
 `;
 
-const GroupChatModalUserList = styled.li`
-  width: 90%;
-  height: 40px;
-  display: flex;
-  margin: 0 auto;
-  align-items: center;
-  justify-content: space-between;
+// const GroupChatModalUserList = styled.li`
+//   width: 90%;
+//   height: 40px;
+//   display: flex;
+//   margin: 0 auto;
+//   align-items: center;
+//   justify-content: space-between;
 
-  &.active {
-    background: #964545;
-  }
-  &:hover {
-    background: red;
-  }
+//   &.active {
+//     background: #964545;
+//   }
+//   &:hover {
+//     background: red;
+//   }
 
-  & .isActive::after {
-    content: '추가';
-    cursor: pointer;
-  }
-  &.active .isActive::after {
-    content: '해제';
-    cursor: pointer;
-  }
-`;
+//   & .isActive::after {
+//     content: '추가';
+//     cursor: pointer;
+//   }
+//   &.active .isActive::after {
+//     content: '해제';
+//     cursor: pointer;
+//   }
+// `;
 
-const AddUserListWrap = styled.div`
-  width: 95%;
-  min-height: 40px;
-  margin: 10px auto;
-  display: flex;
-  flex-wrap: wrap;
-`;
+// const AddUserListWrap = styled.div`
+//   width: 95%;
+//   min-height: 40px;
+//   margin: 10px auto;
+//   display: flex;
+//   flex-wrap: wrap;
+// `;
 
-const AddUserList = styled.li`
-  width: fit-content;
-  height: 30px;
-  padding: 1px 10px;
-  background: #fff;
-  border-radius: 5px;
-  margin-right: 10px;
-  margin-bottom: 10px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+// const AddUserList = styled.li`
+//   width: fit-content;
+//   height: 30px;
+//   padding: 1px 10px;
+//   background: #fff;
+//   border-radius: 5px;
+//   margin-right: 10px;
+//   margin-bottom: 10px;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
 
-  & .cancelUser {
-  }
+//   & .cancelUser {
+//   }
 
-  & .cancelUser:hover {
-    cursor: pointer;
-    font-weight: bold;
-  }
-`;
+//   & .cancelUser:hover {
+//     cursor: pointer;
+//     font-weight: bold;
+//   }
+// `;
 
 const GroupChatList = () => {
   const [showAddGroupChat, setShowAddGroupChat] = useState(false);
@@ -189,10 +192,11 @@ const GroupChatList = () => {
   }, []);
 
   const showUserList = () => {
-    getUserList().then((userList) => {
-      setShowAddGroupChat(true);
-      setGroupChatUserList(userList);
-    });
+    // getUserList().then((userList) => {
+    //   setShowAddGroupChat(true);
+    //   setGroupChatUserList(userList);
+    // });
+    setShowAddGroupChat(true);
   };
 
   const router = useRouter();
@@ -202,72 +206,72 @@ const GroupChatList = () => {
   };
 
   //uid배열과 chatRoomUid를 받아와 uid들의 그룹채팅리스트에 그룹채팅uid를 추가해준다.
-  const updateUsersChatRoomUid = async (uid: string, chatRoomUid: string) => {
-    let myGroupChatListPath = ref(
-      realtimeDbService,
-      `userList/${uid}/myGroupChatList`,
-    );
-    const checkCurrentGroupChatRoomsArr: string[] | null = await (
-      await get(myGroupChatListPath)
-    ).val()?.groupChatUid;
-    //checkCurrentGroupChatRooms이 true일때 false일때(참여한 채팅방이 없는상태)로 분기된다.
-    if (checkCurrentGroupChatRoomsArr) {
-      //기존에 가지고있던 채팅리스트에 새로 생성된 채팅방을 추가하여 set해준다.
-      const updateCurrentGroupChatRoomsArr = [
-        ...checkCurrentGroupChatRoomsArr,
-        chatRoomUid,
-      ];
-      set(myGroupChatListPath, {
-        groupChatUid: updateCurrentGroupChatRoomsArr,
-      });
-    } else {
-      //그룹채팅이 아예 처음 초대됐기때문에 바로 채팅방을 set해준다.
-      set(myGroupChatListPath, {
-        groupChatUid: [chatRoomUid],
-      });
-    }
-  };
+  // const updateUsersChatRoomUid = async (uid: string, chatRoomUid: string) => {
+  //   let myGroupChatListPath = ref(
+  //     realtimeDbService,
+  //     `userList/${uid}/myGroupChatList`,
+  //   );
+  //   const checkCurrentGroupChatRoomsArr: string[] | null = await (
+  //     await get(myGroupChatListPath)
+  //   ).val()?.groupChatUid;
+  //   //checkCurrentGroupChatRooms이 true일때 false일때(참여한 채팅방이 없는상태)로 분기된다.
+  //   if (checkCurrentGroupChatRoomsArr) {
+  //     //기존에 가지고있던 채팅리스트에 새로 생성된 채팅방을 추가하여 set해준다.
+  //     const updateCurrentGroupChatRoomsArr = [
+  //       ...checkCurrentGroupChatRoomsArr,
+  //       chatRoomUid,
+  //     ];
+  //     set(myGroupChatListPath, {
+  //       groupChatUid: updateCurrentGroupChatRoomsArr,
+  //     });
+  //   } else {
+  //     //그룹채팅이 아예 처음 초대됐기때문에 바로 채팅방을 set해준다.
+  //     set(myGroupChatListPath, {
+  //       groupChatUid: [chatRoomUid],
+  //     });
+  //   }
+  // };
 
-  const createGroupChatRoom = () => {
-    //1.한번의 호출로 같은 고유번호를 넣어야하기때문에 미리 선언
-    let chatRoomUid = createChatUid();
+  // const createGroupChatRoom = () => {
+  //   //1.한번의 호출로 같은 고유번호를 넣어야하기때문에 미리 선언
+  //   let chatRoomUid = createChatUid();
 
-    //1-1. addUserList에는 아직 현재유저가 빠져있다.
-    //본인이 만드는 방이기때문에 state에 본인을 추가한다.
-    const addUserListUpdateMe = [...addUserList];
-    addUserListUpdateMe.push({
-      displayName: authService.currentUser.displayName,
-      uid: authService.currentUser.uid,
-    });
+  //   //1-1. addUserList에는 아직 현재유저가 빠져있다.
+  //   //본인이 만드는 방이기때문에 state에 본인을 추가한다.
+  //   const addUserListUpdateMe = [...addUserList];
+  //   addUserListUpdateMe.push({
+  //     displayName: authService.currentUser.displayName,
+  //     uid: authService.currentUser.uid,
+  //   });
 
-    //2.초대된 유저를 순회하며, 각 유저들의 채팅리스트를 업데이트해준다.
-    addUserListUpdateMe.forEach(async (i) => {
-      updateUsersChatRoomUid(i.uid, chatRoomUid);
-    });
-    //2. 고유 그룹채팅방 생성하고, 시작메시지 push하기
-    let groupChatPath = ref(realtimeDbService, `groupChatRooms/${chatRoomUid}`);
-    let groupChatMessagePath = ref(
-      realtimeDbService,
-      `groupChatRooms/${chatRoomUid}/chat`,
-    );
-    //3.채팅방제목을 적으면 그걸로 사용, 안적었을 시 고유번호로 사용
-    let chatRoomTitle =
-      chatRoomsTitleInputRef.current.value !== ''
-        ? chatRoomsTitleInputRef.current.value
-        : chatRoomUid;
+  //   //2.초대된 유저를 순회하며, 각 유저들의 채팅리스트를 업데이트해준다.
+  //   addUserListUpdateMe.forEach(async (i) => {
+  //     updateUsersChatRoomUid(i.uid, chatRoomUid);
+  //   });
+  //   //2. 고유 그룹채팅방 생성하고, 시작메시지 push하기
+  //   let groupChatPath = ref(realtimeDbService, `groupChatRooms/${chatRoomUid}`);
+  //   let groupChatMessagePath = ref(
+  //     realtimeDbService,
+  //     `groupChatRooms/${chatRoomUid}/chat`,
+  //   );
+  //   //3.채팅방제목을 적으면 그걸로 사용, 안적었을 시 고유번호로 사용
+  //   let chatRoomTitle =
+  //     chatRoomsTitleInputRef.current.value !== ''
+  //       ? chatRoomsTitleInputRef.current.value
+  //       : chatRoomUid;
 
-    set(groupChatPath, {
-      chatRoomsTitle: chatRoomTitle,
-      connectedUser: addUserListUpdateMe,
-    });
-    push(groupChatMessagePath, {
-      displayName: authService.currentUser.displayName,
-      uid: authService.currentUser.uid,
-      message: `그룹채팅이 시작되었습니다.`,
-      createdAt: convertDate(Timestamp.fromDate(new Date()).seconds),
-    });
-    setShowAddGroupChat(false);
-  };
+  //   set(groupChatPath, {
+  //     chatRoomsTitle: chatRoomTitle,
+  //     connectedUser: addUserListUpdateMe,
+  //   });
+  //   push(groupChatMessagePath, {
+  //     displayName: authService.currentUser.displayName,
+  //     uid: authService.currentUser.uid,
+  //     message: `그룹채팅이 시작되었습니다.`,
+  //     createdAt: convertDate(Timestamp.fromDate(new Date()).seconds),
+  //   });
+  //   setShowAddGroupChat(false);
+  // };
 
   return (
     <GroupChatContainer>
@@ -299,6 +303,9 @@ const GroupChatList = () => {
         })}
       </GroupListWrap>
       {showAddGroupChat && (
+        <InviteGroupChatModal setShowAddGroupChat={setShowAddGroupChat} />
+      )}
+      {/* {showAddGroupChat && (
         <AddGroupChatModal>
           <>
             채팅방의 이름을 지어주세요.(기본은 랜덤 12자리이며, 언제든지 변경
@@ -360,7 +367,6 @@ const GroupChatList = () => {
                   }}
                 >
                   {i.displayName}
-                  {/* 버튼을 active 시킬 때 가상선택자로 추가 해제를 알려준다 */}
                   <div className='isActive'></div>
                 </GroupChatModalUserList>
               );
@@ -376,7 +382,7 @@ const GroupChatList = () => {
             </button>
           </>
         </AddGroupChatModal>
-      )}
+      )} */}
     </GroupChatContainer>
   );
 };
