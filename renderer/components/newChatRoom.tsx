c8743ed46e1fbddfebfe5169c05930de6a25c17d
qirtudgus <qirtudgus@naver.com>
2023-01-29 13:52:25 +0900

    feat: 모듈 적용 및 채팅방 레이아웃 기능 추가

--- renderer/components/newChatRoom.tsx
+++ renderer/components/newChatRoom.tsx
@@ -1,9 +1,10 @@
 import { off, onValue, push, ref } from 'firebase/database';
 import Head from 'next/head';
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
-import { realtimeDbService } from '../firebaseConfig';
+import { getOneToOneChatListPath, realtimeDbService } from '../firebaseConfig';
 import ChatRoomHeaderTitle from './ChatRoomHeaderTitle';
 import LoadingSpinner from './LoadingSpinner';
+import MessageContainerGroup from './messageContainerGroup';
 import MessageContainerOneToOne from './messageContainerOneToOne';
 import SendMessageInput from './SendMessageInput';
 
@@ -16,21 +17,15 @@ const ChatRoom = ({
 }) => {
   const [chatList, setChatList] = useState([]);
   const [isChatLoading, setIsChatLoading] = useState(false);
+  const getChatListPath = getOneToOneChatListPath(chatRoomUid);
+  const 레이아웃 = localStorage.getItem('oneToOneChatLayout');
 
   //useEffect onValue로 채팅을 계속 가져와야함
   useEffect(() => {
-    console.log(`현재 채팅방 : ${chatRoomUid}`);
-
-    const 채팅경로 = ref(
-      realtimeDbService,
-      `oneToOneChatRooms/${chatRoomUid}/chat`,
-    );
-    onValue(채팅경로, async (snapshot) => {
+    onValue(getChatListPath, (snapshot) => {
       console.log(`채팅이 갱신되었습니다`);
-      console.log(snapshot.val());
       //최신메시지 하나만 가져와서 이어붙이면 좋을거같은데...
-      let messageList = Object.values(await snapshot.val());
-      console.log(messageList);
+      let messageList = Object.values(snapshot.val());
       console.log('로딩완료');
       setIsChatLoading(true);
       setChatList(messageList);
@@ -38,7 +33,7 @@ const ChatRoom = ({
 
     return () => {
       //언마운트시 해당 경로에 대한 관찰자를 off해주면 왔다갔다해도 onValue가 한번씩만 호출됨 (onValue가 쌓이는걸 방지)
-      off(채팅경로);
+      off(getChatListPath);
       setIsChatLoading(false);
       console.log('채팅방을 나갔습니다.');
     };
@@ -60,7 +55,11 @@ const ChatRoom = ({
       </Head>
       <ChatRoomHeaderTitle title={displayName} />
       {isChatLoading ? (
-        <MessageContainerOneToOne chatList={chatList} />
+        레이아웃 === 'oneToOne' ? (
+          <MessageContainerOneToOne chatList={chatList} />
+        ) : (
+          <MessageContainerGroup chatList={chatList} />
+        )
       ) : (
         <LoadingSpinner />
       )}
