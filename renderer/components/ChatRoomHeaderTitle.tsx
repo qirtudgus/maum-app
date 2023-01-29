33d85591250e03f9abb0fe5e68f0a60050bb891f
qirtudgus <qirtudgus@naver.com>
2023-01-29 10:04:56 +0900

    feat: 유저카운트,리스트 모달 구현
    프롭스로 userList를 받았을때만 화면에 출력된다.
    이때 말하는 상황은 그룹채팅에 해당된다.

--- renderer/components/ChatRoomHeaderTitle.tsx
+++ renderer/components/ChatRoomHeaderTitle.tsx
@@ -1,5 +1,9 @@
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { UserList } from '../firebaseConfig';
+import CloseSvg from './svg/closeSvg';
+import PeopleSvg from './svg/peopleSvg';
 
 const ChatTitle = styled.div`
   width: 100%;
@@ -9,28 +13,84 @@ const ChatTitle = styled.div`
   text-align: center;
   font-size: 20px;
   font-weight: bold;
-
+  display: flex;
+  justify-content: center;
   border-bottom: 1px solid ${({ theme }) => theme.colors.borderColor};
   position: relative;
+  display: flex;
+
+  justify-content: center;
+  align-items: center;
   & .closeBtn {
+    height: 100%;
     cursor: pointer;
     position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     right: 10px;
+  }
+`;
+
+const UserListWrap = styled.div`
+  position: absolute;
+  height: fit-content;
+  top: 35px;
+  z-index: 20;
+  padding: 10px 15px;
+  background: #fff;
+  box-shadow: ${({ theme }) => theme.boxShadow};
+  font-size: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  cursor: default;
+`;
+
+const UserListli = styled.li`
+  line-height: 20px;
+  height: 20px;
+  margin-bottom: 3px;
+`;
+
+const UserListButton = styled.div`
+  height: 30px;
+  cursor: pointer;
+  display: flex;
+  user-select: none;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+  margin-left: 10px;
+  & svg {
+    width: 20px;
+    height: 20px;
+  }
+  & .userListCount {
+    font-size: 15px;
   }
 `;
 
 const ChatRoomHeaderTitle = ({
   title,
-}: // ChatStatesetState,
-{
+  userList,
+}: {
   title: string;
-  // ChatStatesetState: React.Dispatch<React.SetStateAction<boolean>>;
+  userList?: UserList[];
 }) => {
   const router = useRouter();
+  const [isOpenUserList, setIsOpenUserList] = useState(false);
+  console.log(router);
+  useEffect(() => {
+    return () => {
+      setIsOpenUserList(false);
+    };
+  }, []);
 
   return (
     <ChatTitle>
-      {title}와의 대화
+      {title}
       <span
         title='닫기'
         className='closeBtn'
@@ -39,8 +99,25 @@ const ChatRoomHeaderTitle = ({
           // ChatStatesetState(false);
         }}
       >
-        X
+        <CloseSvg />
       </span>
+      {userList && (
+        <UserListButton
+          onClick={() => {
+            setIsOpenUserList((prev) => !prev);
+          }}
+        >
+          <PeopleSvg />
+          {isOpenUserList && (
+            <UserListWrap>
+              {userList.map((i) => {
+                return <UserListli>{i.displayName}</UserListli>;
+              })}
+            </UserListWrap>
+          )}
+          <span className='userListCount'>{userList.length}</span>
+        </UserListButton>
+      )}
     </ChatTitle>
   );
 };
