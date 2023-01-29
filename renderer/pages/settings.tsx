070b4054d3a70b6fdd2094c6f46c2ef337c17471
qirtudgus <qirtudgus@naver.com>
2023-01-29 14:45:34 +0900

    feat: 레이아웃 세팅 기능 추가

--- renderer/pages/settings.tsx
+++ renderer/pages/settings.tsx
@@ -1,9 +1,117 @@
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
+import styled from 'styled-components';
+
+const Wrap = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.div`
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
+
+const SubTitle = styled.div`
+  font-size: 14px;
+  font-weight: bold;
+  text-align: center;
+  color: #555;
+  padding-bottom: 5px;
+  border-bottom: 1px solid#eee;
+`;
+
+const CheckWrap = styled.div`
+  width: 500px;
+  height: 300px;
+  background: #eee;
+
+  margin-bottom: 50px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const CheckZone = styled.div`
+  cursor: pointer;
+
+  width: 200px;
+  height: 250px;
+  background: #fff;
+  border-radius: 10px;
+  padding: 10px 10px;
+  position: relative;
+  box-shadow: ${({ theme }) => theme.boxShadow};
+  &:hover {
+    outline: 5px solid ${({ theme }) => theme.colors.main};
+  }
+  & > input {
+    position: absolute;
+    top: 3px;
+    right: 3px;
+    width: 20px;
+    height: 20px;
+  }
+`;
+
+const BarWrap = styled.div`
+  /* margin: 0 auto; */
+`;
+
+const Bar = styled.div`
+  width: 50%;
+  height: 30px;
+  border-radius: 10px;
+  /* margin: 0 auto; */
+  margin-top: 10px;
+  position: relative;
+  background: #888;
+  &:nth-child(1) {
+    left: 90px;
+  }
+  &:nth-child(2) {
+    width: 60%;
+  }
+  &:nth-child(4) {
+    left: 110px;
+    width: 40%;
+  }
+  &:last-child {
+    width: 100%;
+    height: 25px;
+    background: #eee;
+  }
+`;
+
+const GroupBar = styled.div`
+  width: 80%;
+  height: 30px;
+  border-radius: 10px;
+  /* margin: 0 auto; */
+  margin-top: 10px;
+  position: relative;
+  background: #888;
+
+  &:nth-child(2) {
+  }
+  &:nth-child(4) {
+    width: 60%;
+  }
+  &:last-child {
+    width: 100%;
+    height: 25px;
+    background: #eee;
+  }
+`;
 
 const Settings = () => {
   const ref = useRef<HTMLInputElement>();
   const groupRef = useRef<HTMLInputElement>();
-
   const groupLayoutRef = useRef<HTMLInputElement>();
   const groupLayoutRef2 = useRef<HTMLInputElement>();
 
@@ -20,70 +128,127 @@ const Settings = () => {
     }
   }, []);
 
+  const oneToOneSettings = (
+    e: React.ChangeEvent<HTMLInputElement> | string,
+  ) => {
+    if (typeof e === 'string') {
+      localStorage.setItem('oneToOneChatLayout', `${e}`);
+    } else {
+      const value = e.target.value as 'oneToOne' | 'group';
+      localStorage.setItem('oneToOneChatLayout', `${value}`);
+    }
+  };
+
+  const groupSettings = (e: React.ChangeEvent<HTMLInputElement> | string) => {
+    if (typeof e === 'string') {
+      localStorage.setItem('groupChatLayout', `${e}`);
+    } else {
+      const value = e.target.value as 'oneToOne' | 'group';
+      localStorage.setItem('groupChatLayout', `${value}`);
+    }
+  };
+
   return (
-    <>
-      <div>세팅페이지</div>
-      <div>
-        일대일채팅 레이아웃 선택: 일대일 식
-        <input
-          name='oneToOneChatLayout'
-          type='radio'
-          ref={ref}
-          value={'one'}
-          onChange={(e) => {
-            console.log(e.target.checked);
-            console.log(e.target.value);
-            if (e.target.checked) {
-              localStorage.setItem('oneToOneChatLayout', 'oneToOne');
-            }
+    <Wrap>
+      <Title>일대일 채팅 레이아웃 선택</Title>
+      <CheckWrap>
+        <CheckZone
+          onClick={() => {
+            oneToOneSettings('oneToOne');
+            ref.current.checked = true;
           }}
-        ></input>
-        그룹식
-        <input
-          ref={groupRef}
-          name='oneToOneChatLayout'
-          type='radio'
-          value={'group'}
-          onChange={(e) => {
-            console.log(e.target.checked);
-            console.log(e.target.value);
-            if (e.target.checked) {
-              localStorage.setItem('oneToOneChatLayout', 'group');
-            }
+        >
+          일대일식
+          <input
+            name='oneToOneChatLayout'
+            type='radio'
+            ref={ref}
+            value={'oneToOne'}
+            onChange={oneToOneSettings}
+          ></input>
+          <BarWrap>
+            <SubTitle>OOO</SubTitle>
+            <Bar />
+            <Bar />
+            <Bar />
+            <Bar />
+            <Bar />
+          </BarWrap>
+        </CheckZone>
+        <CheckZone
+          onClick={() => {
+            oneToOneSettings('group');
+            groupRef.current.checked = true;
           }}
-        ></input>
-      </div>
-      <div>
-        그룹채팅 레이아웃 선택: 일대일 식
-        <input
-          name='groupChatLayout'
-          type='radio'
-          ref={groupLayoutRef}
-          value={'one'}
-          onChange={(e) => {
-            console.log(e.target.checked);
-            console.log(e.target.value);
-            if (e.target.checked) {
-              localStorage.setItem('groupChatLayout', 'oneToOne');
-            }
+        >
+          그룹식
+          <input
+            ref={groupRef}
+            name='oneToOneChatLayout'
+            type='radio'
+            value={'group'}
+            onChange={oneToOneSettings}
+          ></input>
+          <BarWrap>
+            <SubTitle>OOO</SubTitle>
+            <GroupBar />
+            <GroupBar />
+            <GroupBar />
+            <GroupBar />
+            <GroupBar />
+          </BarWrap>
+        </CheckZone>
+      </CheckWrap>
+      <Title>그룹 채팅 레이아웃 선택</Title>
+      <CheckWrap>
+        <CheckZone
+          onClick={() => {
+            groupSettings('oneToOne');
+            groupLayoutRef.current.checked = true;
           }}
-        ></input>
-        그룹식
-        <input
-          ref={groupLayoutRef2}
-          name='groupChatLayout'
-          type='radio'
-          value={'group'}
-          onChange={(e) => {
-            console.log(e.target.checked);
-            console.log(e.target.value);
-            if (e.target.checked) {
-              localStorage.setItem('groupChatLayout', 'group');
-            }
+        >
+          일대일식
+          <input
+            name='groupChatLayout'
+            type='radio'
+            ref={groupLayoutRef}
+            value={'oneToOne'}
+            onChange={groupSettings}
+          ></input>
+          <BarWrap>
+            <SubTitle>OOO</SubTitle>
+            <Bar />
+            <Bar />
+            <Bar />
+            <Bar />
+            <Bar />
+          </BarWrap>
+        </CheckZone>
+        <CheckZone
+          onClick={() => {
+            oneToOneSettings('group');
+            groupLayoutRef2.current.checked = true;
           }}
-        ></input>
-      </div>
-    </>
+        >
+          그룹식
+          <input
+            ref={groupLayoutRef2}
+            name='groupChatLayout'
+            type='radio'
+            value={'group'}
+            onChange={groupSettings}
+          ></input>
+          <BarWrap>
+            <SubTitle>OOO</SubTitle>
+            <GroupBar />
+            <GroupBar />
+            <GroupBar />
+            <GroupBar />
+            <GroupBar />
+          </BarWrap>
+        </CheckZone>
+      </CheckWrap>
+    </Wrap>
   );
 };
 
