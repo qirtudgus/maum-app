956c2d1b033bd4ee53f0c2c8cdc2c4d4b7dc7298
qirtudgus <qirtudgus@naver.com>
2023-01-30 10:50:07 +0900

    feat: props가 아니라, 라우터쿼리로 데이터를 사용해보았다.

--- renderer/components/newChatRoom.tsx
+++ renderer/components/newChatRoom.tsx
@@ -1,100 +1,142 @@
 import { pushNotifications } from 'electron';
-import { off, onValue, push, ref } from 'firebase/database';
+import { off, onValue, push, ref, set, update } from 'firebase/database';
+import { Timestamp } from 'firebase/firestore';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
-import { authService, getOneToOneChatListPath } from '../firebaseConfig';
+import {
+  authService,
+  getOneToOneChatListPath,
+  realtimeDbService,
+} from '../firebaseConfig';
 import ChatRoomHeaderTitle from './ChatRoomHeaderTitle';
 import LoadingSpinner from './LoadingSpinner';
 import MessageContainerGroup from './messageContainerGroup';
 import MessageContainerOneToOne from './messageContainerOneToOne';
 import SendMessageInput from './SendMessageInput';
 
-const ChatRoom = ({
-  displayName,
-  chatRoomUid,
-}: {
-  displayName: string;
-  chatRoomUid: string;
-}) => {
-  const [chatList, setChatList] = useState([]);
-  const [isChatLoading, setIsChatLoading] = useState(false);
-  const getChatListPath = getOneToOneChatListPath(chatRoomUid);
-  // const 레이아웃 = localStorage.getItem('oneToOneChatLayout');
-  const [레이아웃, 레이아웃설정] = useState('');
-
+const ChatRoom = () =>
+  //   {
+  //   displayName,
+  //   chatRoomUid,
+  //   opponentUid,
+  // }: {
+  //   displayName: string;
+  //   chatRoomUid: string;
+  //   opponentUid: string;
+  // }
   {
-  }
+    const router = useRouter();
+    const displayName2 = router.query.displayName as string;
+    const chatRoomUid2 = router.query.chatRoomUid as string;
+    const opponentUid2 = router.query.opponentUid as string;
+    const [chatList, setChatList] = useState([]);
+    const [isChatLoading, setIsChatLoading] = useState(false);
+    const getChatListPath = getOneToOneChatListPath(chatRoomUid2);
+    // const 레이아웃 = localStorage.getItem('oneToOneChatLayout');
+    const [레이아웃, 레이아웃설정] = useState('');
 
-  interface ChatData {
-    createdAt: string;
-    displayName: string;
-    message: string;
-    uid: string;
-  }
+    console.log('프롭스확인');
 
-  //useEffect onValue로 채팅을 계속 가져와야함
-  useEffect(() => {
-    레이아웃설정(localStorage.getItem('oneToOneChatLayout'));
-    onValue(getChatListPath, (snapshot) => {
-      console.log(`채팅이 갱신되었습니다`);
-      //최신메시지 하나만 가져와서 이어붙이면 좋을거같은데...
-      let messageList: ChatData[] = Object.values(snapshot.val());
-      console.log(messageList);
-      //메시지...가능..아래처럼 분기하면 마지막 메시지가
-      // 내가 보낸게 아닐때만 알람이 뜬다.
-      if (
-        messageList[messageList.length - 1].displayName !==
-        authService.currentUser.displayName
-      ) {
-        const notification = new Notification(`${displayName}`, {
-          body: messageList[messageList.length - 1].message,
+    interface ChatData {
+      createdAt: string;
+      displayName: string;
+      message: string;
+      uid: string;
+    }
+
+    //첫 입장 시, 퇴장 시  접속시간 기록
+    //잘 기록된다.
+
+    useEffect(() => {
+      const 경로 = ref(
+        realtimeDbService,
+        `oneToOneChatRooms/${chatRoomUid2}/connectedUser/${authService.currentUser?.uid}`,
+      );
+      update(경로, {
+        lastConnectTimeStamp: Timestamp.fromDate(new Date()).seconds,
+      });
+
+      // update(경로, {
+      //   [authService.currentUser?.displayName]: Timestamp.fromDate(new Date())
+      //     .seconds,
+      // });
+      return () => {
+        update(경로, {
+          lastConnectTimeStamp: Timestamp.fromDate(new Date()).seconds,
         });
-      }
 
-      console.log('로딩완료');
-      setIsChatLoading(true);
-      setChatList(messageList);
-    });
+        // update(경로, {
+        //   [authService.currentUser?.displayName]: Timestamp.fromDate(new Date())
+        //     .seconds,
+        // });
+      };
+    }, []);
 
-    return () => {
-      //언마운트시 해당 경로에 대한 관찰자를 off해주면 왔다갔다해도 onValue가 한번씩만 호출됨 (onValue가 쌓이는걸 방지)
-      off(getChatListPath);
-      setIsChatLoading(false);
-      console.log('채팅방을 나갔습니다.');
-    };
-  }, [chatRoomUid]);
+    //useEffect onValue로 채팅을 계속 가져와야함
+    useEffect(() => {
+      레이아웃설정(localStorage.getItem('oneToOneChatLayout'));
+      onValue(getChatListPath, async (snapshot) => {
+        console.log(`채팅이 갱신되었습니다`);
+        console.log(snapshot.val());
+        //최신메시지 하나만 가져와서 이어붙이면 좋을거같은데...
+        let messageList: ChatData[] = Object.values(await snapshot.val());
+        console.log(messageList);
+        //메시지...가능..아래처럼 분기하면 마지막 메시지가
+        // 내가 보낸게 아닐때만 알람이 뜬다.
+        // if (
+        //   messageList[messageList.length - 1].displayName !==
+        //   authService.currentUser.displayName
+        // ) {
+        //   const notification = new Notification(`${displayName}`, {
+        //     body: messageList[messageList.length - 1].message,
+        //   });
+        // }
 
-  //언마운트 시 현재 활성화된 메뉴의 액티브돔을 찾아서 제거한다.
-  useLayoutEffect(() => {
-    return () => {
-      if (document.getElementById('chatUserActive')) {
-        document.getElementById('chatUserActive').removeAttribute('id');
-      }
-    };
-  }, []);
+        console.log('로딩완료');
+        setIsChatLoading(true);
+        setChatList(messageList);
+      });
 
-  return (
-    <>
-      <Head>
-        <title>maumTalk - {displayName}</title>
-      </Head>
-      <ChatRoomHeaderTitle title={displayName} />
-      {isChatLoading ? (
-        레이아웃 === 'oneToOne' ? (
-          <MessageContainerOneToOne chatList={chatList} />
+      return () => {
+        //언마운트시 해당 경로에 대한 관찰자를 off해주면 왔다갔다해도 onValue가 한번씩만 호출됨 (onValue가 쌓이는걸 방지)
+        off(getChatListPath);
+        setIsChatLoading(false);
+        console.log('채팅방을 나갔습니다.');
+      };
+    }, [chatRoomUid2]);
+
+    //언마운트 시 현재 활성화된 메뉴의 액티브돔을 찾아서 제거한다.
+    useLayoutEffect(() => {
+      return () => {
+        if (document.getElementById('chatUserActive')) {
+          document.getElementById('chatUserActive').removeAttribute('id');
+        }
+      };
+    }, []);
+
+    return (
+      <>
+        <Head>
+          <title>maumTalk - {displayName2}</title>
+        </Head>
+        <ChatRoomHeaderTitle title={displayName2} />
+        {isChatLoading ? (
+          레이아웃 === 'oneToOne' ? (
+            <MessageContainerOneToOne chatList={chatList} />
+          ) : (
+            <MessageContainerGroup chatList={chatList} />
+          )
         ) : (
-          <MessageContainerGroup chatList={chatList} />
-        )
-      ) : (
-        <LoadingSpinner />
-      )}
-      <SendMessageInput
-        displayName={displayName}
-        chatRoomUid={chatRoomUid}
-        isOneToOneOrGroup='oneToOne'
-      />
-    </>
-  );
-};
+          <LoadingSpinner />
+        )}
+        <SendMessageInput
+          displayName={displayName2}
+          chatRoomUid={chatRoomUid2}
+          isOneToOneOrGroup='oneToOne'
+        />
+      </>
+    );
+  };
 
 export default React.memo(ChatRoom);
