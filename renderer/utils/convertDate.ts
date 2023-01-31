bb23bb6cad9fab806487c307c7c43cd2fd6f0eb2
qirtudgus <qirtudgus@naver.com>
2023-02-01 04:04:44 +0900

    feat: 날짜 변환 옵션 추가

--- renderer/utils/convertDate.ts
+++ renderer/utils/convertDate.ts
@@ -1,9 +1,14 @@
-export function convertDate(time) {
-  //time should be server timestamp seconds only
+export function convertDate(time: number) {
   let dateInMillis = time * 1000;
   let date = new Date(dateInMillis);
-  let myDate = date.toLocaleDateString();
-  let myTime = date.toLocaleTimeString();
-  myDate = myDate.replaceAll('/', '-');
+  let myDate = date.toLocaleDateString([], {
+    year: '2-digit',
+    month: '2-digit',
+    day: '2-digit',
+  });
+  let myTime = date.toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
   return myDate + ' ' + myTime;
 }
