bc9640434aa6969bccbadd7dfddd891d1353d79c
qirtudgus <qirtudgus@naver.com>
2023-02-02 07:41:02 +0900

    feat: 일대일채팅방 svg

---
+++ renderer/components/svg/oneToOneSvg.tsx
@@ -0,0 +1,16 @@
+const OneToOneSvg = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 24 24'
+      height='24px'
+      width='24px'
+      fill='#fff'
+    >
+      <path fill='none' d='M0 0h24v24H0V0z' />
+      <path d='M4 4h16v12H5.2L4 17.2V4m0-2c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2H4zm4.4 4v7.8h-2V6h2zm8.9 0v7.8h-2V6h2zm-4.6 1.6v2.1h-2V7.6h2zm0 3.1v2.1h-2v-2.1h2zM6.5 7.8H5L6.4 6m9 1.8h-1.5L15.3 6' />
+    </svg>
+  );
+};
+
+export default OneToOneSvg;
