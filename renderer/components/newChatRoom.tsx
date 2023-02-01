285e9a52c573fafc385670c9807abaf5a939fb13
qirtudgus <qirtudgus@naver.com>
2023-02-01 10:39:45 +0900

    feat: 불필요한 코드 정리

--- renderer/components/newChatRoom.tsx
+++ renderer/components/newChatRoom.tsx
@@ -25,183 +25,147 @@ import MessageContainerGroup from './messageContainerGroup';
 import MessageContainerOneToOne from './messageContainerOneToOne';
 import SendMessageInput from './SendMessageInput';
 
-const ChatRoom = () =>
-  //   {
-  //   displayName,
-  //   chatRoomUid,
-  //   opponentUid,
-  // }: {
-  //   displayName: string;
-  //   chatRoomUid: string;
-  //   opponentUid: string;
-  // }
-  {
-    const router = useRouter();
-    const displayName2 = router.query.displayName as string;
-    const chatRoomUid2 = router.query.chatRoomUid as string;
-    const opponentUid2 = router.query.opponentUid as string;
-    const [chatList, setChatList] = useState([]);
-    const [isChatLoading, setIsChatLoading] = useState(false);
-    const [ConnectedUsers, setConnectedUsers] = useState([]);
-    const getChatListPath = getOneToOneChatListPath(chatRoomUid2);
-    // const 레이아웃 = localStorage.getItem('oneToOneChatLayout');
-    const [레이아웃, 레이아웃설정] = useState('');
+const ChatRoom = () => {
+  const router = useRouter();
+  const displayName2 = router.query.displayName as string;
+  const chatRoomUid2 = router.query.chatRoomUid as string;
+  const opponentUid2 = router.query.opponentUid as string;
+  const [chatList, setChatList] = useState([]);
+  const [isChatLoading, setIsChatLoading] = useState(false);
+  const [ConnectedUsers, setConnectedUsers] = useState([]);
+  const getChatListPath = getOneToOneChatListPath(chatRoomUid2);
+  // const 레이아웃 = localStorage.getItem('oneToOneChatLayout');
+  const [레이아웃, 레이아웃설정] = useState('');
 
-    //첫 입장 시, 퇴장 시  접속시간 기록
-    //잘 기록된다.
-    //각 ui에 isOn값도 추가해주자 이는 SendInput에서 쓰기위함이다.
-    useEffect(() => {
-      const 경로 = ref(
-        realtimeDbService,
-        `oneToOneChatRooms/${chatRoomUid2}/connectedUser/${authService.currentUser?.uid}`,
-      );
-      update(경로, {
-        displayName: authService.currentUser.displayName,
-        uid: authService.currentUser.uid,
-        lastConnectTimeStamp: Timestamp.fromDate(new Date()).seconds,
-        isOn: true,
-      });
-      //앱 강종시에도 채팅접속상태 Off 해주기
-      onDisconnect(경로).update({
-        isOn: false,
-        lastConnectTimeStamp: Timestamp.fromDate(new Date()).seconds,
-      });
-      //채팅방 나갈 시에 접속상태 off해주기
-      return () => {
-        update(경로, {
-          lastConnectTimeStamp: Timestamp.fromDate(new Date()).seconds,
-          isOn: false,
-        });
-      };
-    }, []);
-
-    //useEffect onValue로 채팅을 계속 가져와야함
-    useEffect(() => {
-      레이아웃설정(localStorage.getItem('oneToOneChatLayout'));
-      onValue(getChatListPath, async (snapshot) => {
-        console.log(`채팅이 갱신되었습니다`);
-        // console.log(snapshot.val());
-        //최신메시지 하나만 가져와서 이어붙이면 좋을거같은데...
-        //메시지가 0개일때 예외처리
-        if (snapshot.val()) {
-          let messageList: ChatDataNew[] = Object.values(await snapshot.val());
-          // console.log(messageList);
-          let messageObj = snapshot.val();
-          console.log(messageObj);
-          let 현재유저uid = authService.currentUser?.uid;
-
-          //요소를 반복하며 ?..
-          for (let property in messageObj) {
-            // console.log(`${property}: ${messageObj[property]}`);
-            // console.log(messageObj[property]);
-            // console.log(messageObj[property].readUsers);
-            let 내가읽었는지결과 =
-              messageObj[property].readUsers[authService.currentUser.uid];
-            //값이 true면 패스, false면 true로 업데이트하는 함수 호출
-            // console.log(내가읽었는지결과);
-
-            if (내가읽었는지결과 === false) {
-              const 업데이트할메시지 = ref(
-                realtimeDbService,
-                `oneToOneChatRooms/${chatRoomUid2}/chat/${property}/readUsers`,
-              );
-
-              update(업데이트할메시지, { [authService.currentUser.uid]: true });
-            }
-          }
-
-          //메시지...가능..아래처럼 분기하면 마지막 메시지가
-          // 내가 보낸게 아닐때만 알람이 뜬다.
-          // if (
-          //   messageList[messageList.length - 1].displayName !==
-          //   authService.currentUser.displayName
-          // ) {
-          //   const notification = new Notification(`${displayName}`, {
-          //     body: messageList[messageList.length - 1].message,
-          //   });
-          // }
-
-          console.log('로딩완료');
-          setIsChatLoading(true);
-          setChatList(messageList);
-        } else {
-          setIsChatLoading(true);
-        }
-      });
-
-      return () => {
-        //언마운트시 해당 경로에 대한 관찰자를 off해주면 왔다갔다해도 onValue가 한번씩만 호출됨 (onValue가 쌓이는걸 방지)
-        off(getChatListPath);
-        setIsChatLoading(false);
-        console.log('채팅방을 나갔습니다.');
-      };
-    }, [chatRoomUid2]);
-
-    //현재 채팅 connectedUser를 onValue하면서 그 결과를 SendInput에 전달해준다.
-    //SendInput은 그 값에 따라서 전송 시 메시지에 true처리할 사람을 결정한다.
-    //접속한 사람이 생겼을때 메시지 다시 렌더링
-    useEffect(() => {
-      const 접속유저경로 = ref(
-        realtimeDbService,
-        `oneToOneChatRooms/${chatRoomUid2}/connectedUser`,
-      );
-      onValue(접속유저경로, (snapshot) => {
-        console.log('방에 접속했다');
-        const 밸류스 = Object.values(snapshot.val());
-        setConnectedUsers(밸류스);
-        //메시지를 가져와서 갱신 시켜주자
-        get(getChatListPath).then((res) => {
-          if (res.val()) {
-            const 메시지배열: ChatDataNew[] = Object.values(res.val());
-            console.log(메시지배열);
-            setChatList(메시지배열);
-          }
-        });
-      });
-
-      return () => {
-        off(접속유저경로);
-      };
-    }, []);
-
-    //언마운트 시 현재 활성화된 메뉴의 액티브돔을 찾아서 제거한다.
-    useLayoutEffect(() => {
-      return () => {
-        if (document.getElementById('chatUserActive')) {
-          document.getElementById('chatUserActive').removeAttribute('id');
-        }
-      };
-    }, []);
-
-    return (
-      <>
-        <Head>
-          <title>maumTalk - {displayName2}</title>
-        </Head>
-        <ChatRoomHeaderTitle title={displayName2} />
-        {isChatLoading ? (
-          레이아웃 === 'oneToOne' ? (
-            <MessageContainerOneToOne chatList={chatList} />
-          ) : (
-            <MessageContainerGroup chatList={chatList} />
-          )
-        ) : (
-          <LoadingSpinner />
-        )}
-        <SendMessageInput
-          connectedUsers={ConnectedUsers}
-          displayName={displayName2}
-          chatRoomUid={chatRoomUid2}
-          opponentUid={opponentUid2}
-          isOneToOneOrGroup='oneToOne'
-        />
-        {ConnectedUsers.map((i) => {
-          // console.log(i);
-          // 채팅방에 접속한 유저들만 렌더링 시켜줄 수 있다.
-          return <div key={i.uid}>{i.isOn ? i.displayName : null}</div>;
-        })}
-      </>
+  //첫 입장 시, 퇴장 시  접속시간 기록
+  //잘 기록된다.
+  //각 ui에 isOn값도 추가해주자 이는 SendInput에서 쓰기위함이다.
+  useEffect(() => {
+    const 경로 = ref(
+      realtimeDbService,
+      `oneToOneChatRooms/${chatRoomUid2}/connectedUser/${authService.currentUser?.uid}`,
     );
-  };
+    update(경로, {
+      displayName: authService.currentUser.displayName,
+      uid: authService.currentUser.uid,
+      lastConnectTimeStamp: Timestamp.fromDate(new Date()).seconds,
+      isOn: true,
+    });
+    //앱 강종시에도 채팅접속상태 Off 해주기
+    onDisconnect(경로).update({
+      isOn: false,
+      lastConnectTimeStamp: Timestamp.fromDate(new Date()).seconds,
+    });
+    //채팅방 나갈 시에 접속상태 off해주기
+    return () => {
+      update(경로, {
+        lastConnectTimeStamp: Timestamp.fromDate(new Date()).seconds,
+        isOn: false,
+      });
+    };
+  }, []);
+
+  //useEffect onValue로 채팅을 계속 가져와야함
+  useEffect(() => {
+    레이아웃설정(localStorage.getItem('oneToOneChatLayout'));
+    onValue(getChatListPath, async (snapshot) => {
+      console.log(`채팅이 갱신되었습니다`);
+      // console.log(snapshot.val());
+      //최신메시지 하나만 가져와서 이어붙이면 좋을거같은데...
+      //메시지가 0개일때 예외처리
+      if (snapshot.val()) {
+        let messageList: ChatDataNew[] = Object.values(await snapshot.val());
+        let messageObj = snapshot.val();
+        //요소를 반복하며 ?..
+        for (let property in messageObj) {
+          // console.log(`${property}: ${messageObj[property]}`);
+          // console.log(messageObj[property]);
+          // console.log(messageObj[property].readUsers);
+          let 내가읽었는지결과 =
+            messageObj[property].readUsers[authService.currentUser.uid];
+          //값이 true면 패스, false면 true로 업데이트하는 함수 호출
+          // console.log(내가읽었는지결과);
+
+          if (내가읽었는지결과 === false) {
+            const 업데이트할메시지 = ref(
+              realtimeDbService,
+              `oneToOneChatRooms/${chatRoomUid2}/chat/${property}/readUsers`,
+            );
+
+            update(업데이트할메시지, { [authService.currentUser.uid]: true });
+          }
+        }
+
+        console.log('로딩완료');
+        setIsChatLoading(true);
+        setChatList(messageList);
+      } else {
+        setIsChatLoading(true);
+      }
+    });
+
+    return () => {
+      //언마운트시 해당 경로에 대한 관찰자를 off해주면 왔다갔다해도 onValue가 한번씩만 호출됨 (onValue가 쌓이는걸 방지)
+      off(getChatListPath);
+      setIsChatLoading(false);
+      console.log('채팅방을 나갔습니다.');
+    };
+  }, [chatRoomUid2]);
+
+  //현재 채팅 connectedUser를 onValue하면서 그 결과를 SendInput에 전달해준다.
+  //SendInput은 그 값에 따라서 전송 시 메시지에 true처리할 사람을 결정한다.
+  useEffect(() => {
+    const 접속유저경로 = ref(
+      realtimeDbService,
+      `oneToOneChatRooms/${chatRoomUid2}/connectedUser`,
+    );
+
+    onValue(접속유저경로, (snapshot) => {
+      console.log('방에 접속했다');
+      const 밸류스 = Object.values(snapshot.val());
+      setConnectedUsers(밸류스);
+      //메시지를 가져와서 갱신 시켜주자
+      get(getChatListPath).then((res) => {
+        if (res.val()) {
+          const 메시지배열: ChatDataNew[] = Object.values(res.val());
+          console.log(메시지배열);
+          setChatList(메시지배열);
+        }
+      });
+    });
+
+    return () => {
+      off(접속유저경로);
+    };
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>maumTalk - {displayName2}</title>
+      </Head>
+      <ChatRoomHeaderTitle title={displayName2} />
+      {isChatLoading ? (
+        레이아웃 === 'oneToOne' ? (
+          <MessageContainerOneToOne chatList={chatList} />
+        ) : (
+          <MessageContainerGroup chatList={chatList} />
+        )
+      ) : (
+        <LoadingSpinner />
+      )}
+      <SendMessageInput
+        connectedUsers={ConnectedUsers}
+        chatRoomUid={chatRoomUid2}
+        isOneToOneOrGroup='oneToOne'
+      />
+      {ConnectedUsers.map((i) => {
+        // console.log(i);
+        // 채팅방에 접속한 유저들만 렌더링 시켜줄 수 있다.
+        return <div key={i.uid}>{i.isOn ? i.displayName : null}</div>;
+      })}
+    </>
+  );
+};
 
 export default React.memo(ChatRoom);
