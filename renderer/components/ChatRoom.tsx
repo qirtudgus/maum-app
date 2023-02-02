7026f0177c81b48c5356a91f01eedf37df105e9f
qirtudgus <qirtudgus@naver.com>
2023-02-02 22:28:00 +0900

    feat: 채팅방 목록 한개를 구성하는 컴포넌트 생성

---
+++ renderer/components/ChatRoom.tsx
@@ -0,0 +1,124 @@
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
+import { convertDate } from '../utils/convertDate';
+import {
+  ChatRoomType,
+  ResultGroupRoom,
+  ResultOneToOneRoom,
+} from '../utils/makeChatRooms';
+import PeopleSvg from './svg/peopleSvg';
+const ChatRoomList = styled.li`
+  cursor: pointer;
+  width: 100%;
+  background: #fff;
+  padding: 10px;
+  height: fit-content;
+  display: flex;
+  &:hover {
+    background: #eee;
+  }
+`;
+const ChatRoomInfo = styled.div`
+  width: 100%;
+`;
+
+const ChatRoomLastMessage = styled.div`
+  display: flex;
+  justify-content: space-between;
+  color: #444;
+  width: 100%;
+  word-break: break-all;
+`;
+const ChatRoomNotReadCount = styled.div`
+  padding: 3px 5px;
+  text-align: center;
+  color: #fff;
+  font-size: 15px;
+  font-weight: bold;
+  border-radius: 10px;
+  background: #d61818;
+  width: fit-content;
+  height: fit-content;
+  flex-shrink: 0;
+`;
+const ChatRoomTitleAndTime = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
+  & .title {
+    font-size: 15px;
+    font-weight: bold;
+  }
+
+  & .timeStamp {
+    font-size: 15px;
+    color: #555;
+  }
+`;
+
+const ChatIcon = styled.div`
+  position: relative;
+  width: 45px;
+  height: 45px;
+  flex-shrink: 0;
+  background: #d0ddff;
+  border-radius: 10px;
+  margin-right: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & svg {
+    width: 30px;
+    height: 30px;
+    fill: #fff;
+  }
+`;
+
+const ChatRoom = ({
+  chatRoom,
+  chatRoomType,
+}: {
+  chatRoom: ResultOneToOneRoom | ResultGroupRoom;
+  chatRoomType: ChatRoomType;
+}) => {
+  const router = useRouter();
+
+  return (
+    <>
+      <ChatRoomList
+        key={chatRoom.chatRoomUid}
+        onClick={() => {
+          router.push(
+            chatRoomType === 'oneToOne'
+              ? `/oneToOneChatRooms/oneToOne?displayName=${chatRoom.displayName}&chatRoomUid=${chatRoom.chatRoomUid}`
+              : `/groupChatRooms/group?chatRoomsTitle=${chatRoom.displayName}&chatRoomUid=${chatRoom.chatRoomUid}`,
+
+            //   `/chatRooms/oneToOne?displayName=${item?.opponentName}&chatRoomUid=${item?.chatRoomUid}`,
+          );
+        }}
+      >
+        <ChatIcon>
+          <PeopleSvg />
+        </ChatIcon>
+        <ChatRoomInfo>
+          <ChatRoomTitleAndTime>
+            <span className='title'>{chatRoom?.displayName}</span>
+            <span className='timeStamp'>
+              {convertDate(chatRoom.createdSecondsAt)}
+            </span>
+          </ChatRoomTitleAndTime>
+          <ChatRoomLastMessage>
+            <div className='message'>{chatRoom.lastMessage}</div>
+            {chatRoom.notReadCount !== 0 && (
+              <ChatRoomNotReadCount>
+                {chatRoom.notReadCount}
+              </ChatRoomNotReadCount>
+            )}
+          </ChatRoomLastMessage>
+        </ChatRoomInfo>
+      </ChatRoomList>
+    </>
+  );
+};
+
+export default ChatRoom;
