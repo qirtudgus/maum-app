3bc35a8ca4d9222ec3fa642508e6abfe3f3586c3
qirtudgus <qirtudgus@naver.com>
2023-02-03 00:46:01 +0900

    feat: jsdoc 추가

--- renderer/utils/convertDate.ts
+++ renderer/utils/convertDate.ts
@@ -1,3 +1,8 @@
+/**
+ * firebase timeStamp로 생성한 seconds를 날짜형식으로 반환해주는 함수입니다.
+ * @param time 초로 계산된 number를 사용합니다.
+ * @returns  1675337799 => "23. 02. 02. 오후 08:36"
+ */
 export function convertDate(time: number) {
   let dateInMillis = time * 1000;
   let date = new Date(dateInMillis);
