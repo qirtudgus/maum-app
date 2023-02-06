f4fc74a00d4123e99db34f9d945d9d161f0e6149
qirtudgus <qirtudgus@naver.com>
2023-02-06 14:37:06 +0900

    refactor: 함수 최적화

--- renderer/utils/checkBlankValue.ts
+++ renderer/utils/checkBlankValue.ts
@@ -3,11 +3,13 @@
  * @param value 공백이 있는지 검사할 string
  * @returns
  */
-export const checkBlankValue = (value: string) => {
-  const blank_pattern = /^\s+\s+$/g;
-  if (value === ' ' || value.length === 0 || blank_pattern.test(value)) {
-    return true;
-  } else {
-    return false;
-  }
-};
+// export const checkBlankValue = (value: string) => {
+//   const blank_pattern = /^\s+\s+$/g;
+//   if (value === ' ' || value.length === 0 || blank_pattern.test(value)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+export const checkBlankValue = (value: string) => !value.trim();
